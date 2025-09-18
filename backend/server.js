adb65d8b4d4d6f4b302e1e9ccf54d20755543d78
Thanuja Jinadasa <thanuj.tj@gmail.com>
2025-09-18 15:52:00 -0400

    adding docker files

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:8050',
+  origin: process.env.FRONTEND_URL || 'https://library.tjbookrequests.org/login',
   credentials: true
 }));
 app.use(express.json());
